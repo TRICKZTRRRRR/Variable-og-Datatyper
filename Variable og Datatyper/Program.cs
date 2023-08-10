3b88f130360697d15e6ca0f1faacb3174a58df03
TRICKZTRRRRR <jonas04@outlook.dk>
2023-08-10 10:01:10 +0200

    Add project files.

---
+++ Variable og Datatyper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_og_Datatyper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = 16;
+            double width = 5.1;
+
+            int Area = lenght * width;
+            double d = lenght * width;
+
+            Console.WriteLine(Area.ToString());
+            Console.ReadLine();
+
+            // I opgave B kører koden ikke, fordi den ikke kan konventere en int til en double
+
+            // I opgave C kører koden, fordi den godt kan konventere en int til en double
+        }
+    }
+}
